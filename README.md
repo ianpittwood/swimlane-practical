2c12f177167a956a7810636adfe4079b97052a39
Ian H. Pittwood <ipittwood@condata.com>
2022-06-01 20:09:10 -0700

    Create a chart for deploying the Node app
    - Create basic helm chart including node app and mongodb
    - Add main.tf terraform definition for deploy k8s and helm chart

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Swimlane Practical
+
+## Preparing Local Environment
+Local environment requires functional copies of `helm` and `kubectl` with a 
+local kubernetes cluster setup such as `minikube`. Instructions for each
+can be found below:
+- [kubectl install](https://minikube.sigs.k8s.io/docs/start/)
+- [minikube start](https://minikube.sigs.k8s.io/docs/start/)
+- [helm install](https://helm.sh/docs/intro/install/)
+
+The `terraform` binary will be necessary to deploy via Terraform. Instructions
+can be found below:
+- [terraform install](https://learn.hashicorp.com/tutorials/terraform/install-cli)
+
+## Deploy from Chart
+The chart can be deployed using `helm` directly with the following command:
+
+    helm upgrade --install swimlanepracticalchart ./swimlanepracticalchart
+
+## Deply from Terraform
+The application can be deployed using `terraform` with the following commands:
+
+    terraform init
+    terraform plan
+    terraform apply
--- main.tf
+++ main.tf
@@ -1,10 +1,66 @@
+variable "storage_path" {
+  type = string
+  default = "/tmp/mongo"
+}
+
+variable "node_host_name" {
+  type = string
+  default = "minikube"
+}
+
 provider "kubernetes" {
   config_path    = "~/.kube/config"
 }
 
-resource "kubernetes_namespace" "minikube-namespace1" {
+resource "kubernetes_namespace" "swimlanepractical" {
     metadata {
-        name = "learn-terraform"
+        name = "swimlanepractical"
+    }
+}
+
+resource "kubernetes_persistent_volume_claim" "pvc-mongodb" {
+    metadata {
+      name = "pvc-mongodb"
+      namespace = "${kubernetes_namespace.swimlanepractical.metadata.0.name}"
+    }
+    spec {
+      storage_class_name = "local"
+      access_modes = ["ReadWriteOnce"]
+      resources {
+          requests = {
+              storage = "5Gi"
+          }
+      }
+      volume_name = "${kubernetes_persistent_volume.pv-mongodb.metadata.0.name}"
+    }
+}
+
+resource "kubernetes_persistent_volume" "pv-mongodb" {
+    metadata {
+      name = "pv-mongodb"
+    }
+    spec {
+        capacity = {
+          "storage" = "10Gi"
+        }
+        storage_class_name = "local"
+        access_modes = ["ReadWriteOnce"]
+        persistent_volume_source {
+          local {
+              path = var.storage_path
+          }
+        }
+        node_affinity {
+            required {
+                node_selector_term {
+                    match_expressions {
+                        key = "kubernetes.io/hostname"
+                        operator = "In"
+                        values = [var.node_host_name]
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -14,7 +70,23 @@ provider "helm" {
     }
 }
 
-resource "helm_release" "local" {
-    name = "buildachart"
-    chart = "./buildachart"
+resource "helm_release" "swimlanepracticalchart" {
+    name = "swimlanepracticalchart"
+    chart = "./swimlanepracticalchart"
+    namespace = "${kubernetes_namespace.swimlanepractical.metadata.0.name}"
+
+    set {
+        name = "mongodb.persistence.enabled"
+        value = "true"
+    }
+
+    set {
+        name = "mongodb.livelinessProbe.initialDelaySeconds"
+        value = 180
+    }
+
+    set {
+        name = "mongodb.persistence.existingClaim"
+        value = "${kubernetes_persistent_volume_claim.pvc-mongodb.metadata.0.name}"
+    }
 }
